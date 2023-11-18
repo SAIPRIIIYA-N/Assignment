1af8a5a4699901ad929fe75398f067e903aecbff
SAIPRIIIYA-N <saipriyan589@gmail.com>
2023-11-18 23:05:06 +0530

    New project

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("part1").onclick = () => alert("red");
+document.getElementById("part2").onclick = () => alert("blue");
+document.getElementById("part3").onclick = () => alert("yellow");
+document.getElementById("part4").onclick = () => alert("green");
